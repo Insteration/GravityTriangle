bd4f34c5e1fa026e51d88c8dfec0f4bc26523177
Art Karma <inste@icloud.com>
2019-04-30 13:07:39 +0300

    Initial Commit

---
+++ GravityTriangle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GravityTriangle
+//
+//  Created by Art Karma on 4/30/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
